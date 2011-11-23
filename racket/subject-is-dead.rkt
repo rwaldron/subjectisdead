5fee42505921098461aab2d4f60fce9a8d23be0c
Rick Waldron <waldron.rick@gmail.com>
2011-11-22 21:08:27 -0500

    remove leading space from ..(let

--- racket/subject-is-dead.rkt
+++ racket/subject-is-dead.rkt
@@ -1,4 +1,4 @@
 #lang racket
 (define subject "[Subject] is dead, long live [Subject]")
 (define counter 1)
-	(let loop () (when (> counter 0) (display (regexp-replace* #px"\\[Subject\\]" subject subject))) (loop)))
+(let loop () (when (> counter 0) (display (regexp-replace* #px"\\[Subject\\]" subject subject))) (loop)))
